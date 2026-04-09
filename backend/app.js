9053396c559abcb5966db7ebb9566004f39bf969
sidsingh2311 <sid02012001@gmail.com>
2026-04-09 11:54:16 +0530

    fix the url

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,12 @@ import interviewRoutes from "./src/routes/interview.routes.js"
 // creating an instance of the express
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [
+        process.env.FRONTEND_URL,
+        "http://localhost:5173"
+    ],
     credentials: true
-}))
+}));
 app.use(express.json({limit:"200kb"}));
 app.use(express.urlencoded({limit: "200kb", extended: true}));
 app.use(express.static("public")); 
